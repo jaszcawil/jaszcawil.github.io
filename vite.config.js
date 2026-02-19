4a36537cd117fc252322e775f3f2ed3b4579fe6f
Jasz <jcawil@meetsoci.com>
2026-02-19 15:22:59 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/jaszcawil.github.io",
+
+  esbuild: {
+    loader: 'jsx'
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
