dd73313f60e283b7d5af68b9e57a0ace3b893857
Jasz <jcawil@meetsoci.com>
2026-02-21 00:24:07 +0800

    Jasz's Portfolio test

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-
-  esbuild: {
-    loader: 'jsx'
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 });
